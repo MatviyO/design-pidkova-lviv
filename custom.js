696ebec7b8a1537f9285978b4e665bc649f0d7c9
Oleh <oleh.m97@gmail.com>
2021-01-25 09:52:25 +0200

    jghjgh

--- custom.js
+++ custom.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         get: 'user',
         limit: 12,
         userId: '2872087102',
-        accessToken: IGQVJWYzUzZAFV0LXZAQd3g1bkJER3Y2YVNIUTN2a1k5TTFYT1d2c3gxdlptTU16T1hFZA19tSEJ0emlzX3ZAhT0wtSXcyOHE3eGJqcXNkaUczbl8xWVFfWjdtWklfTUxhV253bXUxX1R6WThOb1dRN0JjUwZDZD,
+        accessToken: 'IGQVJWYzUzZAFV0LXZAQd3g1bkJER3Y2YVNIUTN2a1k5TTFYT1d2c3gxdlptTU16T1hFZA19tSEJ0emlzX3ZAhT0wtSXcyOHE3eGJqcXNkaUczbl8xWVFfWjdtWklfTUxhV253bXUxX1R6WThOb1dRN0JjUwZDZD',
         transform: function(item) {
             var d = new Date(item.timestamp);
             item.date = [d.getDate(), d.getMonth(), d.getYear()].join('/');
